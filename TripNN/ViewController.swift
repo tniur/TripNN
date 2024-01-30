ce9289171b549dde6eb247a6d97db7cee910891d
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-01-30 21:14:19 +0300

    Initial Commit

---
+++ TripNN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripNN
+//
+//  Created by Pavel on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
