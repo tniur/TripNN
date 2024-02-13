1406ad5043ca62834819601becdef9cf8cb504cf
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-02-14 00:32:16 +0300

    add some views

---
+++ TripNN/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extensions.swift
+//  TripNN
+//
+//  Created by Pavel on 30.01.2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var tripnnOrange: UIColor { UIColor(named: "TripNN Orange") ?? UIColor.orange }
+    static var tripnnWhite: UIColor { UIColor(named: "TripNN White") ?? UIColor.white }
+    static var tripnnYellow: UIColor { UIColor(named: "TripNN Yellow") ?? UIColor.yellow }
+    static var tripnnDark: UIColor { UIColor(named: "TripNN Dark") ?? UIColor.black }
+}
